d5120b1bd99b6a9f4095074af065504eb418d68f
unknown <muhammedarshad898@gmail.com>
2025-05-11 17:25:27 +0530

    'fourth'

--- app/api/transactions/[id]/route.ts
+++ app/api/transactions/[id]/route.ts
@@ -1,16 +1,19 @@
 import Transaction from "@/models/transactionmodels";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { jwtmiddleware } from "@/app/lib/jwtmiddleware";
 
 // DELETE /api/transactions/[id]
-export async function DELETE(req: Request, context: { params: { id: string } }) {
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     const result = await jwtmiddleware(req);
     if (result.valid === false) return result.response;
 
-    const { id } = context.params;
-
+    const { id } = params;
     const deletedTransaction = await Transaction.findByIdAndDelete(id);
+
     return NextResponse.json({ success: true, deletedTransaction });
   } catch (error: any) {
     return NextResponse.json({
@@ -22,12 +25,15 @@ export async function DELETE(req: Request, context: { params: { id: string } }) 
 }
 
 // PUT /api/transactions/[id]
-export async function PUT(req: Request, context: { params: { id: string } }) {
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   try {
     const result = await jwtmiddleware(req);
     if (result.valid === false) return result.response;
 
-    const { id } = context.params;
+    const { id } = params;
     const { title, amount, type, date } = await req.json();
 
     if (!title || !amount || !type || !date) {
@@ -53,4 +59,3 @@ export async function PUT(req: Request, context: { params: { id: string } }) {
     });
   }
 }
-
