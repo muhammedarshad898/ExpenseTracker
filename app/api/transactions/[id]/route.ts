ee584b8646d9e1f921a91cae0221eab0d6845e47
unknown <muhammedarshad898@gmail.com>
2025-05-11 17:54:26 +0530

    'fifth'

--- app/api/transactions/[id]/route.ts
+++ app/api/transactions/[id]/route.ts
@@ -1,39 +1,14 @@
-import Transaction from "@/models/transactionmodels";
 import { NextRequest, NextResponse } from "next/server";
+import Transaction from "@/models/transactionmodels";
 import { jwtmiddleware } from "@/app/lib/jwtmiddleware";
 
-// DELETE /api/transactions/[id]
-export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  try {
-    const result = await jwtmiddleware(req);
-    if (result.valid === false) return result.response;
-
-    const { id } = params;
-    const deletedTransaction = await Transaction.findByIdAndDelete(id);
-
-    return NextResponse.json({ success: true, deletedTransaction });
-  } catch (error: any) {
-    return NextResponse.json({
-      success: false,
-      message: "Internal Server Error",
-      error: error.message,
-    });
-  }
-}
-
 // PUT /api/transactions/[id]
-export async function PUT(
-  req: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function PUT(req: NextRequest, context: any) {
   try {
     const result = await jwtmiddleware(req);
-    if (result.valid === false) return result.response;
+    if (!result.valid) return result.response;
 
-    const { id } = params;
+    const { id } = context.params;
     const { title, amount, type, date } = await req.json();
 
     if (!title || !amount || !type || !date) {
@@ -59,3 +34,24 @@ export async function PUT(
     });
   }
 }
+
+// DELETE /api/transactions/[id]
+export async function DELETE(req: NextRequest, context: any) {
+  try {
+    const result = await jwtmiddleware(req);
+    if (!result.valid) return result.response;
+
+    const { id } = context.params;
+
+    const deletedTransaction = await Transaction.findByIdAndDelete(id);
+    return NextResponse.json({ success: true, deletedTransaction });
+  } catch (error: any) {
+    return NextResponse.json({
+      success: false,
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+}
+
+
